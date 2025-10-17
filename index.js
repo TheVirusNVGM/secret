bf66d5e029425509c4eaffa6f03ae3a8e99e5032
maleshevmax <maleshevmax@gmail.com>
2025-10-17 03:35:19 +0300

    Add routing for /app/1757350/TWITCH-PHOBIA/ path

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 export default {
-  async fetch(request) {
-    return new Response(await fetch("index.html"), {
-      headers: {"content-type": "text/html"}
-    });
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    
+    // Перенаправляем главную страницу на /app/1757350/TWITCH-PHOBIA/
+    if (url.pathname === '/' || url.pathname === '') {
+      return Response.redirect(url.origin + '/app/1757350/TWITCH-PHOBIA/', 301);
+    }
+    
+    // Показываем index.html для пути /app/1757350/TWITCH-PHOBIA/
+    if (url.pathname === '/app/1757350/TWITCH-PHOBIA/' || url.pathname === '/app/1757350/TWITCH-PHOBIA') {
+      return env.ASSETS.fetch(new URL('/index.html', url.origin));
+    }
+    
+    // Для всех остальных запросов используем assets
+    return env.ASSETS.fetch(request);
   }
 }
